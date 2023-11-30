fe717ea90209056872a203bd8316eedaeaf4ba2d
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2023-12-01 00:14:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let str="";
+	for(let i=0;i<shortHex.length;i++)
+		{
+			if(shortHex[i]!='#'){
+				str+=shortHex[i]+shortHex[i];
+			}
+		}
+	return '#'+str;
+	
 };
 
 // Do not change the code below.
